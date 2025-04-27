866f19173f87a3a1a58d18c1c89d16cbf08e872b
Ranjan-S1227 <ranjan.s.1227@gmail.com>
2025-04-27 13:57:50 +0530

    few deletions

--- app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignupForm from '@/components/SignupForm';
-
-export default function SignupPage() {
-  return (
-    <div className="min-h-screen flex justify-center items-center p-4">
-      <SignupForm />
-    </div>
-  );
-}
